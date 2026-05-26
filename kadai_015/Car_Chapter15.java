dfc6e8e3f524a4320bff3171ba7c9101c39b99e1
ksghnhmwww <kiwamu1997@gmail.com>
2026-05-26 22:25:45 +0900

    kadai_015をアップロード

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,28 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	// フィールド
+	private int  gear = 1;
+	private int speed = 10;
+	
+	// ギアの値により速度を変えるメソッド
+	public void changeGear(int afterGear) {
+		this.speed = switch(afterGear) {
+			case 1 -> 10;
+			case 2 -> 20;
+			case 3 -> 30;
+			case 4 -> 40;
+			case 5 -> 50;
+			default -> 10;
+		};
+		
+		System.out.println("ギア" + this.gear + 
+							"から" + afterGear + "に切り替えました");
+	}
+	
+	// ギアチェンジ後の速度を表示するメソッド
+	public void run () {
+		System.out.println("速度は時速" + this.speed + "kmです");
+	}
+
+}
